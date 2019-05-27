33edc35abf9156fea2959c1f2ba78cf24d9a2092
Ayleen <islandordaz@gmail.com>
2019-05-27 16:47:39 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,10 +7,12 @@ const studentController = require('../controllers/student');
 
 // This is for testing that a proper connection was extablished between the front/back ends
 // DELETE THIS ONCE WE ESTABLISH A PROPER FRONT/BACK END CONNECTION 
-router.get('/', (req, res, next)=> res.send('We did it!'));
-
+router.get('/', function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.send('We did it!');
+});
 // Login routes
-router.get('/login', loginController.login);
+router.post('/login', loginController.login);
 
 // Get a student's information
 router.get('/getStudentName', studentController.getStudentName);
