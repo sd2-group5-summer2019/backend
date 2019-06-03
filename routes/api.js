3175a97e734aea6f45c0c0c56ed013c63aff25cb
befcak <vbrusco30794@gmail.com>
2019-06-03 18:17:57 -0400

    Register is now working.
    Input example
    {
    	"username" : "user3",
    	"password" : "pass1",
    	"type" : "admin",
    	"first_name" : "first",
    	"last_name" : "last",
    	"email" : "email@email.com"
    }
    
    Output examples
     --SUCCESSFUL INSERT--
    {
        "status": "Success"
    }
    
    --DUPLICATE USER--
    {
        "err": "Duplicate"
    }
    
    --OTHER ERROR--
    {
       "status" : "unknown error"
    }

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Controllers
 const loginController = require('../controllers/login');
 const studentController = require('../controllers/student');
+const registerController = require('../controllers/register');
 
 // Login routes
 router.post('/login', loginController.login);
+router.post('/register', registerController.register);
 
 // Get a student's information
 router.get('/getStudentName', studentController.getStudentName);
