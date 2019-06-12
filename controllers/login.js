d78abd22b90011c6f3bef90cb674ebd7e7ca7030
Kevin <kevrp369@yahoo.com>
2019-06-11 22:08:59 -0400

    updated package.json and login

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,7 @@
 const { sequelize } = require('../models');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
+const bcrypt = require('bcrypt');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
@@ -13,16 +14,29 @@ function tokenForUser(user) {
   return token;
 }
 
+// ASYNC/AWAIT function to compare user input password vs the db hash
+async function comparePassword(inputPassword, dbHash, res) {
+    const match = await bcrypt.compare(inputPassword, dbHash)
+
+    if(match) {
+        res.send({ status: "Success" });
+    } else {
+        res.send({ status: "Failure: Incorrect Password" });
+    }
+}
+
 class login {
     static async login(req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         const { username, password } = req.body;
 
-        await sequelize.query(`SELECT * FROM users WHERE username = ? AND password = ?`, {replacements:[username, password], type: sequelize.QueryTypes.SELECT})
+        // Only query the username in the db
+        await sequelize.query(`SELECT * FROM users WHERE username = ?`, {replacements:[username], type: sequelize.QueryTypes.SELECT})
             .then(result => {
-                if(result[0] !== undefined) {
-                    if(result[0].username === username && result[0].password === password) {
-                        res.send({ status: "Success" });
+                if(result[0] !== undefined) {  
+                    if (result[0]['username'] === username) {
+                        // Compare user entered password to DB hashed password  
+                        comparePassword(password, result[0]['password'], res);  
                     }
                 }
                 else {
