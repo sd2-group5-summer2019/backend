11096e8db19d090319c3f7f945151c036ab77198
Kevin <kevrp369@yahoo.com>
2019-06-11 22:35:57 -0400

    login_secure done with hash passwords

--- controllers/login.js
+++ controllers/login.js
@@ -6,7 +6,6 @@ const bcrypt = require('bcrypt');
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
   var payload = { username: user };
-  console.log('This is the username', user);
   var token = jwt.sign(payload, config.secret, {
     expiresIn: '30 days',
     algorithm: 'HS256'
@@ -20,6 +19,17 @@ async function comparePassword(inputPassword, dbHash, res) {
 
     if(match) {
         res.send({ status: "Success" });
+    } else {
+        res.send({ status: "Failure: Incorrect Password" });
+    }
+}
+
+// ASYNC/AWAIT function to compare user input password vs the db hash (WITH TOKEN)
+async function comparePasswordSecure(inputPassword, dbHash, token, res) {
+    const match = await bcrypt.compare(inputPassword, dbHash)
+
+    if(match) {
+        res.send({ token });
     } else {
         res.send({ status: "Failure: Incorrect Password" });
     }
@@ -51,13 +61,13 @@ class login {
         const { username, password } = req.body;
 
         const token = tokenForUser(username);
-        console.log(token);
-
-        await sequelize.query(`CALL login(?,?)`, {replacements:[username, password], type: sequelize.QueryTypes.CALL})
+    
+        await sequelize.query(`SELECT * FROM users WHERE username = ?`, {replacements:[username], type: sequelize.QueryTypes.SELECT})
             .then(result => {
-                if(result[0] !== undefined) {
-                    if(result[0].username === username && result[0].password === password) {
-                        res.send({ token });
+                if(result[0] !== undefined) {  
+                    if (result[0]['username'] === username) {
+                        // Compare user entered password to DB hashed password  
+                        comparePasswordSecure(password, result[0]['password'], token, res);  
                     }
                 }
                 else {
