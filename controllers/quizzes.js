3fa20b3cab1186c578cfcf5d74af1926287fbf44
fjoseph631 <frantzcito@knights.ucf.edu>
2019-07-10 13:33:38 -0400

    joseph branch

---
+++ controllers/quizzes.js
@@ -0,0 +1,101 @@
+const { sequelize } = require('../models');
+const config = require('../config/config');
+class quizzes
+{
+    static async quizGrader(req,res,next)
+    {
+        const {form_id,user_id,instance_id}=req.body;
+        var keys = await sequelize.query(
+            'CALL get_quiz_key_answers(?)', 
+            {replacements:[ formid ], type: sequelize.QueryTypes.CALL})
+            .then(result => {
+                // Placeholder for now
+                console.log(result[0]);
+                res.send({ result });
+            })
+            .catch(error => {
+                console.log(error);
+                res.send({ status: "Failed" });
+        }); 
+        var responses = await sequelize.query(
+            'CALL get_user_survey_answers(?,?)', 
+            {replacements:[ form_id,user_id ], type: sequelize.QueryTypes.CALL})
+            .then(result => {
+                // Placeholder for now
+                console.log(result[0]);
+                res.send({ result });
+            })
+            .catch(error => {
+                console.log(error);
+                res.send({ status: "Failed" });
+        }); 
+        var a = quizzes.length;
+        var b = responses.length;
+        for (var i=0;i=a;i++)
+        {
+            for (var j=0;ij=b;j++)
+            {
+                if(responses[j].question_id>keys[i].question_id)
+                {
+                    break;
+                }
+                if(responses[j].question_id==keys[i].question_id)
+                {
+                    //Based on question type
+                    //Multi-Absolute Compare
+                    if(responses[j].answer_text==keys[i].key_text)
+                    {
+                        c+=1;
+                        break;
+                    }
+                    //Short answer - levenstein & dice coefficient
+                    // Long answer - do nothing
+                }
+    
+            }
+        }
+    
+        var grade=c/a*100;
+        //Insert Grades
+        var responses = await sequelize.query(
+            'Update form_instances SET grade = ? where instance_id =?', 
+            {replacements:[ grade,instance_id ], type: sequelize.QueryTypes.Update})
+            .then(
+            )
+            .catch(error => {
+                console.log(error);
+                res.send({ status: "Failed" });
+        });
+    }
+    static async questionTagResults (req,res,next)
+    {
+    //Get question with tag 
+        const {category_id}=req.body;
+        var questions = await sequelize.query(
+        'CALL get_responses_to_question_tag(?)',
+        {replacements:[ category_id ], type: sequelize.QueryTypes.CALL})
+        .then(result => {
+            // Placeholder for now
+            
+            res.send({ result });
+        })
+        .catch(error => {
+            console.log(error);
+            res.send({ status: "Failed" });
+        });
+        var a =questions.length;
+        var c=0;
+        for(var i=0;i<a;i++)
+        {
+            if(results[i].answer_text==results[i].key_text)
+            {
+                c+=1;
+            }
+        }
+    }  
+    //Get respons
+}
+
+//Calc results
+//Return results w/ average
+module.exports=quizzes;
---
+++ controllers/registerCSV.js
@@ -0,0 +1,97 @@
+/*let mysql = require('mysql');*/
+const { sequelize } = require('../models');
+
+const parse = require('csv-parser');
+const uuid4 = require('uuid4');
+const config = require('../config/config');
+class register_csv {
+	static async registercsv(req,res,next)
+	//async function parser(filepath)
+	{
+		const{ file,user_id }=req.body;
+		res={};
+		var array=[];
+		var department_id;
+		var department=await sequelize.query(`Call get_coordinator_department(?)`,
+			{replacements:[user_id]
+			, type: sequelize.QueryTypes.CALL})
+			.then().catch(error => { res.send({ status : "Insertion Failure" }); 
+		});	
+		if (department[0]!=undefined)
+		{
+			department_id=department[0]['id'];
+		}
+		stream = fs.createReadStream(file);
+		stream 
+		.pipe(parse())
+		.on('data', (row) => {
+		var user= JSON.stringify(row);
+		user.replace("\xFFFD", "").replace('"',);
+		
+		var split = user.split(",");
+		var name=split[1].split(" ");
+		var email = split[2].slice(split[2].indexOf(':"')+1).replace("}","");	
+		var nid=split[0].slice(split[0].indexOf(':"')+1);
+		var f_name=name[0].slice(name[0].indexOf(':"')+1).slice(1);
+		if(f_name =='' || name[1]=='' || email=='' || nid =='')
+		{
+			(result => { res.send({ status : "Values Missing" })});
+		}
+		if(!emailIsValid(email))
+		{
+			(result => { res.send({ status : "Invalid email" })});
+		}	
+		var use = {
+			"nid":id.split('"').join(''),
+			"first_name" : f_name,
+			"last_name" : name[1],
+			"email":email.split('"').join('')
+			};
+			//Reject header
+		if (use.nid!="NID")
+			array.push(use);		
+		})
+		.on('end', () => {
+			
+			
+			 
+		
+		});
+		await insert_students(array);
+		// (result => { res.send({ status : "Sucessfull Request" })});
+		//console.log(array);
+	}
+	
+	
+}
+
+
+//Actual Insertion
+async function insert_students(arr)
+{
+	for (i=0;i<arr.length;i++)
+	{
+		var sql = `CALL insert_user (?,?,?,?,?,?)`;
+		await sequelize.query(sql,
+			{replacements:[arr[i].nid,uuid4(),`student`,arr[i].first_name,arr[i].last_name
+			,arr[i].email]})
+			/*, type: sequelize.QueryTypes.CALL})*/
+			.then(result => { res.send({ return : "Success" });
+			}).catch(error => { res.send({ status : "Insertion Failure" }); 
+		});	
+		var id = results[0].user_id;
+		await sequelize.query('call insert_student(?,?,?,?,?,?,?)',
+			{replacements:[id,null,null,department_id ]
+			, type: sequelize.QueryTypes.CALL})
+			.then(result => { res.send({ return : "Success" });
+			}).catch(error => { res.send({ status : "Insertion Failure" }); 
+		});
+	}
+}
+//Regular expression to validate emails	
+function emailIsValid (email) {
+		return /\S+@\S+\.\S+/.test(email)
+}
+//var arr=[];
+module.exports = register_csv;
+	  
