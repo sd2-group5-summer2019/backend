b55bdbd1ac5503299a098bf2c8fcaa71050bd933
Kevin <kevrp369@yahoo.com>
2019-07-15 18:01:16 -0400

    changepassword

--- controllers/login.js
+++ controllers/login.js
@@ -2,6 +2,7 @@ const { sequelize } = require('../models');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
@@ -12,6 +13,17 @@ function tokenForUser(user) {
   });
   return token;
 }
+
+// Return true if oldPass is valid
+// Return false if not valid.
+async function changePassword(oldPass, dbHash) {
+    console.log('Checking Password');
+    const match = await bcrypt.compare(oldPass, dbHash);
+
+    if(match) return true;
+
+    return false;
+}  
 
 // ASYNC/AWAIT function to compare user input password vs the db hash
 async function comparePassword(inputPassword, dbHash, res) {
@@ -82,6 +94,49 @@ class login {
                 }
             })
             .catch(error => console.log(error));
+    }
+
+    static async changePassword(req, res, next) {
+        
+        const { username, oldPass, newPass } = req.body;
+        console.log(req.body);
+
+        try {
+            let result = await sequelize.query(`SELECT * FROM users WHERE username = ?`, {replacements:[username], type: sequelize.QueryTypes.SELECT});
+
+            console.log('Got user');
+            if(result[0] !== undefined) {
+                if(result[0]['username'] === username) {
+                    let change = await changePassword(oldPass, result[0]['password']);
+
+                    console.log(change);
+                    if(!change) {
+                        res.send({ status: "Incorrect Password" });
+                    }
+                    else {
+                        // Generate a salt and then hash the password
+                        const salt = await bcrypt.genSalt(saltRounds);
+                        const hash = await bcrypt.hash(newPass, salt);
+
+                        console.log(hash);
+                        // Insert "hash" as the new password for the user
+                        try {
+                            await sequelize.query(`UPDATE users set password = ? WHERE username = ?`, {replacements:[hash, username], type: sequelize.QueryTypes.UPDATE});
+                            console.log('Inserted new hash');
+                            res.send({ status: "Success" });
+                        }
+                        catch(error) {
+                            console.log(error);
+                            next;
+                        }
+                    }
+                }
+            }
+        }
+        catch(error) {
+            console.log(error);
+            res.send({ status: "MySQL Error" });
+        }
     }
 }
 
